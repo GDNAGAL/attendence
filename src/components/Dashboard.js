658f836961c14a2ced4a1cc8b22c038615e30857
GDNAGAL <gdnagal1536@gmail.com>
2023-04-03 17:11:11 +0530

    update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,7 +9,7 @@ export default function Dashboard() {
       <div id="page-inner">
         <div className="row">
           <div className="col-md-12">
-            <h2>Blank Page</h2>
+            <h2>Dashboard</h2>
             <h5>Welcome Jhon Deo , Love to see you back. </h5>
           </div>
         </div>
